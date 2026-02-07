1f787b0a61669c35755bd4abf95fae40ead0db0f
Ankit Kumar <ankitkumar@pinacalabs.com>
2026-02-07 19:59:17 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  base: '/Portfolio-on-react/',   // 👈 IMPORTANT (repo name)
   server: {
     allowedHosts: ['c92215eb2af0.ngrok-free.app'],
   },
